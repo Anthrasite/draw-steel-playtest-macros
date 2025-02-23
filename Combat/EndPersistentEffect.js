0b9168e5ce0e7c6922071cd289ef1e65acda170a
Cole Kehler <cole4jc@gmail.com>
2025-02-22 23:36:30 -0600

    Add handling for abilities with persistent effects

---
+++ Combat/EndPersistentEffect.js
@@ -0,0 +1,17 @@
+try {
+  if (!actor)
+    ui.notifications.error(`No token is selected`);
+  else {
+    await game.macros.getName(`UpdateAttribute`).execute({
+      attributeName: `persistentCost`,
+      value: 0
+    });
+
+    if (actor.sheet.rendered)
+      actor.sheet.render(true);
+  }
+}
+catch (error) {
+  ui.notifications.error(error);
+  throw error;
+}
