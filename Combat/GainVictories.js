c501007f6d6e3e82e67bb31eb14b5d26bd2c5fef
Cole Kehler <cole4jc@gmail.com>
2025-02-23 22:40:32 -0600

    Add a macro for gaining victories

---
+++ Combat/GainVictories.js
@@ -0,0 +1,15 @@
+try {
+  await game.macros.getName("ValidateActorAttributes").execute();
+
+  const victoriesGained = Number(await game.macros.getName(`ShowSimpleInputDialog`).execute({ title: `Victories`, label: `Victories gained`, defaultValue: 1, allowNegative: false }));
+
+  if (victoriesGained > 0)
+    await game.macros.getName(`UpdateAttribute`).execute({ attributeName: `victories`, value: victoriesGained, isDelta: true });
+
+  if (actor.sheet.rendered)
+    actor.sheet.render(true);
+}
+catch (error) {
+  if (error.message !== "The Dialog was closed without a choice being made.")
+    ui.notifications.error(error);
+}
