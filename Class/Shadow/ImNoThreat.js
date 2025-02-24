7862d590bc54c688ad142396e2cff0134ba4ddc0
Cole Kehler <cole4jc@gmail.com>
2025-02-23 23:08:27 -0600

    Add the ability to add custom functionality when using an ability, and increase the surges when using I'm No Threat

--- Class/Shadow/ImNoThreat.js
+++ Class/Shadow/ImNoThreat.js
@@ -7,5 +7,8 @@ await game.macros.getName("ShareAbility").execute({
   target: "Self",
   effect: "When you use this ability, you cover yourself in an illusion that causes you to appear nonthreatening and harmless to your enemies. You might take on the appearance of a harmless animal of your size, such as a sheep or capybara, or you might appear as a less heroic, unarmed, and capable version of yourself. While this illusion lasts, your strikes made against other creatures gain an edge. If you use this ability in combat, you gain a surge when you use it.</br>The illusion ends when you harm another creature, when you and any creature physically interact, when you use this ability again, or when you end the illusion (no action required).",
   extraResourceCost: "1",
-  extraResourceEffect: "Choose a creature whose size is no more than 1 greater than yours, and who is within 10 squares of you. This ability’s illusion makes you appear to be that creature. This illusion covers your entire body, including clothing and armor, and changes your voice to sound like the creature. You gain an edge on tests made to convince the creature’s allies that you are the creature."
+  extraResourceEffect: "Choose a creature whose size is no more than 1 greater than yours, and who is within 10 squares of you. This ability’s illusion makes you appear to be that creature. This illusion covers your entire body, including clothing and armor, and changes your voice to sound like the creature. You gain an edge on tests made to convince the creature’s allies that you are the creature.",
+  onUseFunc: async function() {
+    await game.macros.getName("UpdateAttribute").execute({ attributeName: "surges", value: 1, isDelta: true });
+  }
 });
