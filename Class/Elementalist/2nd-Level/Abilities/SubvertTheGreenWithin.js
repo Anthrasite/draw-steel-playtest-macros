c0525aeffc21c5318928f11e56bbd341713533f9
Cole Kehler <cole4jc@gmail.com>
2025-02-25 02:19:20 -0600

    Create macros for 2nd level Elementalist abilities

---
+++ Class/Elementalist/2nd-Level/Abilities/SubvertTheGreenWithin.js
@@ -0,0 +1,14 @@
+await game.macros.getName("ShareAbility").execute({
+  name: "Subvert the Green Within",
+  resourceCost: 5,
+  flavorText: "Burrow into their brains and take control!",
+  type: "Action",
+  keywords: "Green, Magic, Ranged, Strike, Void",
+  distance: "Ranged 10",
+  target: "One creature",
+  powerRollStat: "Reason",
+  tier1Effect: "5 + R poison damage",
+  tier2Effect: "9 + R poison damage",
+  tier3Effect: "12 + R poison damage",
+  effect: "The target uses their signature ability against a target of your choice. You then make a power roll against the target of this ability."
+});
