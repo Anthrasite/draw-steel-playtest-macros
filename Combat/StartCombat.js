64be4a122a4a43356385bf5e73b6bb49636a7190
Cole Kehler <cole4jc@gmail.com>
2025-02-25 01:22:09 -0600

    Fix broken StartCombat macro

--- Combat/StartCombat.js
+++ Combat/StartCombat.js
@@ -3,13 +3,13 @@ try {
 
   const victories = (await game.macros.getName(`GetAttribute`).execute({ attributeName: `victories` })).value;
 
+  const resource = await game.macros.getName(`UpdateAttribute`).execute({ attributeName: `resource`, value: victories });
+
   const roll = await new Roll(`${victories}[victories]`).evaluate();
   await roll.toMessage({
     speaker: ChatMessage.implementation.getSpeaker({actor}),
     flavor: `Initial ${resource.label}`
   });
-
-  const resource = await game.macros.getName(`UpdateAttribute`).execute({ attributeName: `resource`, value: victories });
 
   if (actor.sheet.rendered)
     actor.sheet.render(true);
