ea1d81ba96b7b474dd5c32273d638b21ceaa95e5
Cole Kehler <cole4jc@gmail.com>
2025-02-28 04:07:27 -0600

    Update handling of persistent costs to ensure that multiple persistent effects can be maintained at once

--- Combat/EndPersistentEffect.js
+++ Combat/EndPersistentEffect.js
@@ -1,8 +1,15 @@
 try {
   await game.macros.getName("ValidateActorAttributes").execute();
 
-  await game.macros.getName(`UpdateAttribute`).execute({ attributeName: `persistentCost`, value: 0 });
+  const resourceLabel = (await game.macros.getName(`GetAttribute`).execute({ attributeName: `resource` })).label;
+  const selectedAbilities = await game.macros.getName("ShowPersistentCostDialog").execute({ label: `Persistent effects to end`, resourceLabel });
+  if (!selectedAbilities)
+    ui.notifications.info(`No active persistent effects!`);
+  else
+    for (const abilityName of selectedAbilities)
+      await game.macros.getName(`UpdatePersistentCost`).execute({ abilityName });
 }
 catch (error) {
-  ui.notifications.error(error);
+  if (error.message !== "The Dialog was closed without a choice being made.")
+    ui.notifications.error(error);
 }
---
+++ Utility/ShowPersistentCostDialog.js
@@ -0,0 +1,30 @@
+const label = await game.macros.getName(`ValidateParameter`).execute({ name: `label`, value: scope.label, type: `string` });
+const resourceLabel = await game.macros.getName(`ValidateParameter`).execute({ name: `resourceLabel`, value: scope.resourceLabel, type: `string` });
+const selectByDefault = (await game.macros.getName(`ValidateParameter`).execute({ name: `selectByDefault`, value: scope.selectByDefault, type: `boolean`, nullable: true })) ?? false;
+
+const persistentCosts = await game.macros.getName(`GetPersistentCost`).execute();
+if (!Object.keys(persistentCosts).length)
+  return undefined;
+
+let persistCostOptions = ``;
+let counter = 0;
+for (const [abilityName, cost] of Object.entries(persistentCosts))
+  persistCostOptions += `
+    <div>
+      <input id="persistentCost${counter++}" type="checkbox" style="vertical-align: middle;" name="persistentCosts" value="${abilityName}"${selectByDefault ? ` checked` : ``}/>
+      <label style="vertical-align: middle;" for="persistentCost${counter}">${abilityName} <i>(${cost} ${resourceLabel})</i></label>
+    </div>`;
+
+return await Dialog.prompt({
+  title: `Persistent cost`,
+  content: `
+    <form>
+      <div class="form-group">
+        <fieldset>
+          <legend>${label}:</legend>
+          ${persistCostOptions}
+        </fieldset>
+      </div>
+    </form>`,
+  callback: html => html.find('input[name="persistentCosts"]:checked').get().map(i => i.value)
+});
