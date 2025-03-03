18604f061aae9bbd20953a167efe2552ab51e88d
Cole Kehler <cole4jc@gmail.com>
2025-03-03 09:35:59 -0600

    Add sound effect when rolling initiative

--- macros/Combat/RollInitiative.js
+++ macros/Combat/RollInitiative.js
@@ -1,6 +1,8 @@
 //@id=hi1QF7EwgHD3eLVn
 //@name=Roll initiative
 //@img=icons/skills/movement/figure-running-gray.webp
+AudioHelper.play({src: "modules/draw-steel-playtest-macros/audio/sword-drawing.mp3", volume: 1, autoplay: true, loop: false}, true);
+
 const roll = await new Roll(`1d10`).evaluate();
 const heroesActFirst = roll.total >= 6;
 await game.macros.getName(`ShareRoll`).execute({
