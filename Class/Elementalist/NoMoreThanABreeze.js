2ea2384b4cf82b3b7e39397e043caeac7906660a
Anthrasite <Anthrasite@users.noreply.github.com>
2025-03-06 20:29:53 -0600

    Update NoMoreThanABreeze macro to have a persistent cost instead of an extra cost

--- Class/Elementalist/NoMoreThanABreeze.js
+++ Class/Elementalist/NoMoreThanABreeze.js
@@ -7,6 +7,6 @@ await game.macros.getName("ShareAbility").execute({
   distance: "Ranged 10",
   target: "Self or one ally",
   effect: "Until the start of your next turn, the target can move through solid matter, ignores difficult terrain, and their movement can’t provoke opportunity attacks. If the target ends their turn inside solid matter, they are shunted out into the space where they entered it and this effect ends.",
-  extraResourceCost: "1",
-  extraResourceEffect: "The effect lasts until the start of your next turn."
+  persistentCost: "1",
+  persistentEffect: "The effect lasts until the start of your next turn."
 });
