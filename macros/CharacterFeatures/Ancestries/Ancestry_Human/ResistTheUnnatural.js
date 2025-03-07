d804c1dcaea88dac300b660abcdbd2aba0dfbb36
Anthrasite <Anthrasite@users.noreply.github.com>
2025-03-06 20:30:01 -0600

    Fix human ancestry macros using the wrong image

--- macros/CharacterFeatures/Ancestries/Ancestry_Human/ResistTheUnnatural.js
+++ macros/CharacterFeatures/Ancestries/Ancestry_Human/ResistTheUnnatural.js
@@ -1,6 +1,6 @@
 //@id=fX8m9UIvPdH0F5pJ
 //@name=Resist the Unnatural
-//@img=icons/magic/earth/strike-fist-stone.webp
+//@img=icons/environment/people/commoner.webp
 await game.macros.getName("ShareFeature").execute({
   name: "Resist the Unnatural",
   description: "Your connection to the natural world protects you from unnatural forces. When you take damage that isn’t untyped, you can use your triggered action to half the damage.",
--- macros/CharacterFeatures/Ancestries/Ancestry_Human/StayingPower.js
+++ macros/CharacterFeatures/Ancestries/Ancestry_Human/StayingPower.js
@@ -1,6 +1,6 @@
 //@id=WgVqK132pAhlfE7M
 //@name=Staying Power
-//@img=icons/magic/earth/strike-fist-stone.webp
+//@img=icons/environment/people/commoner.webp
 await game.macros.getName("ShareFeature").execute({
   name: "Staying Power",
   description: "Your human anatomy allows you to fight, run, and stay awake longer than others. Increase your number of Recoveries by 2.",
