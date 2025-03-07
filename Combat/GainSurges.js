05ad2a26cdc33fd772e39e964a67959da094a38d
Anthrasite <Anthrasite@users.noreply.github.com>
2025-03-06 20:29:52 -0600

    Add a macro for gaining surges

---
+++ Combat/GainSurges.js
@@ -0,0 +1,15 @@
+try {
+  await game.macros.getName("ValidateActorAttributes").execute();
+
+  const surgesGained = Number(await game.macros.getName(`ShowSimpleInputDialog`).execute({ title: `Surges`, label: `Surges gained`, defaultValue: 1, allowNegative: false }));
+
+  if (surgesGained > 0)
+    await game.macros.getName(`UpdateAttribute`).execute({ attributeName: `surges`, value: surgesGained, isDelta: true });
+
+  if (actor.sheet.rendered)
+    actor.sheet.render(true);
+}
+catch (error) {
+  if (error.message !== "The Dialog was closed without a choice being made.")
+    ui.notifications.error(error);
+}
