67b67038b0d5fd23347240d26603b4542fdca75d
Anthrasite <Anthrasite@users.noreply.github.com>
2025-03-06 20:30:01 -0600

    Update script for generating .db files to use default icons based on the directory, try to get the name from the macro code if it exists, and check for duplicate ids

--- macros/GenerateDB.ps1
+++ macros/GenerateDB.ps1
@@ -1,7 +1,9 @@
 $packsJson = @()
+$macroIds = @()
 
 Get-ChildItem $PSScriptRoot -Directory -Recurse | ForEach-Object {
     $dirPath = $_
+    $dirName = Split-Path $_ -Leaf
     $dbFileContents = ""
 
     # Delete all existing db files
@@ -17,35 +19,49 @@ Get-ChildItem $PSScriptRoot -Directory -Recurse | ForEach-Object {
         $fileContents = Get-Content $_
         $updateFile = $false
 
-        # Get the existing icon path or use the default icon path
-        $imgPath = "icons/svg/dice-target.svg"
+        # Get the existing icon path or use a default icon path based on the directory
+        $imgPath = $null
         $imgMatches = Select-String -Path $_ '//@img=([^\r\n]*)' -AllMatches
         if ($imgMatches.Length -gt 0)
             { $imgPath = $imgMatches[0].Matches[0].Groups[1].Value }
-        else
-        {
+        else {
+            $imgPath = switch ($dirName) {
+                "Ancestry_Devil" { "icons/creatures/unholy/demon-female-succubus-orange.webp" }
+                "Ancestry_Dwarf" { "icons/magic/earth/strike-fist-stone.webp" }
+                "Ancestry_Human" { "icons/environment/people/commoner.webp" }
+                "Class_Censor" { "icons/magic/holy/barrier-shield-winged-blue.webp" }
+                "Class_Elementalist" { "icons/magic/nature/beam-hand-leaves-green.webp" }
+                "Class_Shadow" { "icons/magic/unholy/silhouette-robe-evil-power.webp" }
+                "Complications" { "icons/magic/control/debuff-chains-shackles-movement-blue.webp" }
+                "Kits" { "icons/equipment/shield/buckler-boss-iron-wood-brown.webp" }
+                "Perks" { "icons/magic/light/hand-sparks-glow-yellow.webp" }
+                default { "icons/svg/dice-target.svg" }
+            }
+
             $fileContents = ,"//@img=${imgPath}" + $fileContents
             $updateFile = $true
         }
 
-        # Get the existing name or use the file name
+        # Get the existing name, try to get the name from the macro, or use the file name
         $name = $_.BaseName
         $nameMatches = Select-String -Path $_ '//@name=([^\r\n]*)' -AllMatches
         if ($nameMatches.Length -gt 0)
             { $name = $nameMatches[0].Matches[0].Groups[1].Value }
-        else
-        {
+        else {
+            $nameMatches = Select-String -Path $_ '^\s*name:\s*["`]([^"`]*)["`]' -AllMatches
+            if ($nameMatches.Length -gt 0)
+                { $name = $nameMatches[0].Matches[0].Groups[1].Value }
+
             $fileContents = ,"//@name=${name}" + $fileContents
             $updateFile = $true
         }
 
         # Get the existing id from the file, or generate a new id and prepend it to the file
-        $id = ""
+        $id = $null
         $idMatches = Select-String -Path $_ '//@id=([^\r\n]*)' -AllMatches
         if ($idMatches.Length -gt 0)
             { $id = $idMatches[0].Matches[0].Groups[1].Value }
-        else
-        {
+        else {
             $charSet = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
             $id = (Get-Random -Count 16 -InputObject $charSet) -join ''
 
@@ -53,8 +69,15 @@ Get-ChildItem $PSScriptRoot -Directory -Recurse | ForEach-Object {
             $updateFile = $true
         }
 
+        # Ensure this id doesn't already exist
+        if ($macroIds -contains $id) {
+            Write-Host "Error: Multiple macros with id $id"
+            Exit
+        }
+        $macroIds += $id
+
         # Get the file contents as a single string, escaping slashes, quotes, and newlines
-        $command = $fileContents | ForEach-Object {
+        $command = $fileContents | ForEach-Object{
             $_.replace("\", "\\").replace("`"", "\`"")
         } | Join-String -Separator "\n"
 
@@ -68,7 +91,7 @@ Get-ChildItem $PSScriptRoot -Directory -Recurse | ForEach-Object {
     if ($dbFileContents.length -gt 0) {
         $packPath = ($_ | Resolve-Path -Relative).substring(2)
 
-        $packName = Split-Path $_ -Leaf
+        $packName = $dirName
         $packLabel = $packName.replace("_", " - ")
         $packDBFileName = "${packName}_macros.db"
 
