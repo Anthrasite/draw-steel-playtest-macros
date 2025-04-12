2885723bd4ec008bf70664c3d631c52a8c6be934
Anthrasite <Anthrasite@users.noreply.github.com>
2025-04-12 04:06:31 -0500

    Add commands to the CopyDB script to copy the module.json file

--- packs/_CopyDB.ps1
+++ packs/_CopyDB.ps1
@@ -1,5 +1,8 @@
 $foundryVTTInstall = "$env:LOCALAPPDATA\FoundryVTT\Data\modules\draw-steel-playtest-macros"
-Remove-Item "${foundryVTTInstall}\packs\*" -Recurse -Force
+Remove-Item "$foundryVTTInstall\packs\*" -Recurse -Force
 Get-ChildItem $PSScriptRoot -Filter *.db | ForEach-Object {
   Copy-Item $_ -Destination "$foundryVTTInstall\packs"
 }
+
+Remove-Item "$foundryVTTInstall\module.json"
+Copy-Item "$($PSScriptRoot)\..\module.json" -Destination $foundryVTTInstall
