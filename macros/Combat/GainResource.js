bf33bff7fa78395d3ff8c60156051ac02dc8a7df
Anthrasite <Anthrasite@users.noreply.github.com>
2025-04-17 02:22:58 -0500

    Remove roll from Gain resource macro, as it was confusing and unnecessary

--- macros/Combat/GainResource.js
+++ macros/Combat/GainResource.js
@@ -9,16 +9,8 @@ try {
   const resourceLabel = resource.label.capitalize();
   const resourceGain = Number(await game.macros.getName(`ShowSimpleInputDialog`).execute({ title: resourceLabel, label: `${resourceLabel} gained`, defaultValue: 1, allowNegative: false }));
 
-  if (resourceGain > 0) {
-    const roll = await new Roll(`${resource.value} + ${resourceGain}`).evaluate();
-    await game.macros.getName(`ShareRoll`).execute({
-      activeActor,
-      roll,
-      flavor: resourceLabel
-    });
-
+  if (resourceGain > 0)
     await game.macros.getName(`UpdateAttribute`).execute({ activeActor, attributeName: `resource`, value: resourceGain, isDelta: true });
-  }
 }
 catch (error) {
   if (error.message !== "The Dialog was closed without a choice being made.")
